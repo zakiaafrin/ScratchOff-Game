8eaec245a90361a4bc4717f25169a5d883e6663c
Zakia Afrin Jeme <zjeme@techlaunch.io>
2018-11-28 21:36:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var greyBox = document.querySelectorAll('.grey');
+var win = document.getElementById("win");
+var myModal = document.getElementById("myModal");
+
+function scratch(grey) {
+   grey.src = "scratching.gif"; 
+   setTimeout(() => grey.style.opacity="0", 1000);
+   win.play();
+   setTimeout(() => win.play(), 1000);
+   myModal.style.visibility="hidden";
+   setTimeout(() => myModal.style.visibility="visible", 1500);
+}
+greyBox.forEach(function (grey) {
+   grey.addEventListener("click", function () {
+       scratch(grey);
+   })
+})
